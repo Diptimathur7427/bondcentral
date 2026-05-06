2084170c4545d5fcdbf11bd23b08dc285d2cab3d
Dipti Mathur <diptim705@gmail.com>
2026-05-06 15:59:19 +0530

    added Procfile

--- backend/app.py
+++ backend/app.py
@@ -540,10 +540,9 @@ def seed_data():
 
 # ─── ENTRY POINT ─────────────────────────────────────────────────────────────
 
-
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
