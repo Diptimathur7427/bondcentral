174bedefe76edf9f3c77bc399dabfc9233c9d6c2
Dipti Mathur <diptim705@gmail.com>
2026-05-06 13:18:39 +0530

    backend ready for render

--- backend/app.py
+++ backend/app.py
@@ -540,8 +540,11 @@ def seed_data():
 
 # ─── ENTRY POINT ─────────────────────────────────────────────────────────────
 
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed_data()
-    app.run(debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
